1843d4c6ee7e8527071264778afa8f7f3b7fe60e
bow <sayamon.sittiprom@gmail.com>
2020-06-05 12:32:12 +0700

    week 2 adding icons ans favorites and comments

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -17,10 +17,10 @@ class Contact extends Component {
                     wrapperStyle={{margin: 20}}
                     title='Contact Information'
                    >
-                    <Text> 1 Nucamp Way</Text>
+                    <Text>1 Nucamp Way</Text>
                     <Text>Seattle, WA 98001</Text>
-                    <Text>U.S.A.</Text>
-                    <Text></Text>
+                    <Text style ={{marginBottom:10}}>U.S.A.</Text>
+                   
                     <Text>Phone: 1-206-555-1234</Text>
                     <Text>Email: campsites@nucamp.co</Text>
                 </Card>
