ad4ebe29711196d3efdd667de5d61568e31916d5
bow <sayamon.sittiprom@gmail.com>
2020-06-07 23:36:54 +0700

    Task 3

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -3,7 +3,8 @@ import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet } from 'rea
 import { Card, Icon, Input, Rating } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
-import { postFavorite } from '../redux/ActionCreators';
+import { postFavorite, postComment } from '../redux/ActionCreators';
+
 
 
 const mapStateToProps = state => {
@@ -15,7 +16,9 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProps = {
-    postFavorite: campsiteId => (postFavorite(campsiteId))
+    postFavorite: campsiteId => (postFavorite(campsiteId)),
+    postComment: (campsiteId, rating, author, text) => (postComment(campsiteId, rating, author, text))
+   
 };
 
 
@@ -115,8 +118,8 @@ class CampsiteInfo extends Component {
     }
 
     handleComment(campsiteId) {
-        console.log(JSON.stringify(this.state));
-        this.toggleModal()
+        this.props.postComment(campsiteId,this.state.rating,this.state.author,this.state.text);
+        this.toggleModal();
     }
 
     resetForm() {
